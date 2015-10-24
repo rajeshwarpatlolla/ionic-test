6fe6315022cd8f80d263783b6b030fc707a1b06a
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-10-24 21:09:52 +0530

    sample home page added

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -1,6 +1,23 @@
 angular.module('starter.controllers')
   .controller('HomeCtrl', HomeCtrl);
 
-function HomeCtrl() {
+function HomeCtrl($timeout, $ionicScrollDelegate, ScrollService) {
+
+  try {
+    $timeout(function () {
+      var sv = $ionicScrollDelegate.$getByHandle('quantity_scroll_delegate').getScrollView();
+      ScrollService.handleScroll(sv);
+    });
+    $timeout(function () {
+      var sv = $ionicScrollDelegate.$getByHandle('color_scroll_delegate').getScrollView();
+      ScrollService.handleScroll(sv);
+    });
+    $timeout(function () {
+      var sv = $ionicScrollDelegate.$getByHandle('share_scroll_delegate').getScrollView();
+      ScrollService.handleScroll(sv);
+    });
+  } catch (e) {
+    console.log('Error in scrolling : HomeCtrl')
+  }
 
 }
