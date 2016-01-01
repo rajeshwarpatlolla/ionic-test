a0e57ccaec2cb624c53a3159912a599c16c30fe4
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-01-01 23:45:43 +0530

    fb oauth added

--- www/js/controllers/login.controller.js
+++ www/js/controllers/login.controller.js
@@ -1,10 +1,20 @@
 angular.module('starter.controllers')
   .controller('LoginCtrl', LoginCtrl);
 
-function LoginCtrl($scope, $state) {
+function LoginCtrl($scope, $state, $cordovaOauth) {
 
   $scope.changeState = function (state) {
     $state.go(state);
+  };
+
+  $scope.loginWithFacebook = function (state) {
+    $cordovaOauth
+      .facebook("486208661587668", ["email"])
+      .then(function (result) {
+        console.log(result);
+      }, function (error) {
+        console.log(error);
+      });
   };
 
 }
