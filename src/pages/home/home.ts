dd0237782e250980ffdf6448b575ebe12f9f1d6b
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-10-06 07:35:28 +0530

    modal code removed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { ModalController, NavController, Platform, NavParams, ViewController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,66 +8,8 @@ import { ModalController, NavController, Platform, NavParams, ViewController } f
 })
 export class HomePage {
 
-  constructor(public modalCtrl: ModalController) {
+  constructor(public navCtrl: NavController) {
 
   }
 
-	openModal(characterNum) {
-    let modal = this.modalCtrl.create(ModalsContentPage, characterNum);
-    modal.present();
-  }
-
-}
-
-
-
-@Component({
-  templateUrl: 'modal-content.html'
-})
-export class ModalsContentPage {
-  character;
-
-  constructor(
-      public platform: Platform,
-      public params: NavParams,
-      public viewCtrl: ViewController
-  ) {
-    var characters = [
-      {
-        name: 'Gollum',
-        quote: 'Sneaky little hobbitses!',
-        image: 'assets/img/avatar-gollum.jpg',
-        items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'River Folk' },
-          { title: 'Alter Ego', note: 'Smeagol' }
-        ]
-      },
-      {
-        name: 'Frodo',
-        quote: 'Go back, Sam! I\'m going to Mordor alone!',
-        image: 'assets/img/avatar-frodo.jpg',
-        items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'Shire Folk' },
-          { title: 'Weapon', note: 'Sting' }
-        ]
-      },
-      {
-        name: 'Samwise Gamgee',
-        quote: 'What we need is a few good taters.',
-        image: 'assets/img/avatar-samwise.jpg',
-        items: [
-          { title: 'Race', note: 'Hobbit' },
-          { title: 'Culture', note: 'Shire Folk' },
-          { title: 'Nickname', note: 'Sam' }
-        ]
-      }
-    ];
-    this.character = characters[this.params.get('charNum')];
-  }
-
-  dismiss() {
-    this.viewCtrl.dismiss();
-  }
 }
