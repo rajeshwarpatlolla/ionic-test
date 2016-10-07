1fea22cf92f78f91f29a9a5f0d6a5e0b7d0616aa
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-10-07 14:39:03 +0530

    actionsheet and popup added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, ActionSheetController, AlertController, ModalController, ViewController, NavParams } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,90 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform,
+    public actionsheetCtrl: ActionSheetController,
+    public alerCtrl: AlertController,
+    public modalCtrl: ModalController
+  ) { }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Albums',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present()
 
   }
 
+  openAlert() {
+    let alert = this.alerCtrl.create({
+      title: 'New Friend!',
+      message: 'Your friend, Obi wan Kenobi, just approved your friend request!',
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
+
+  openModal() {
+    let modal = this.modalCtrl.create(ModalsContentPage);
+    modal.present();
+  }
+
+}
+
+
+@Component({
+  template: 'modal-content.html'
+})
+export class ModalsContentPage {
+
+  constructor(
+      public params: NavParams,
+      public viewCtrl: ViewController
+  ) { }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
