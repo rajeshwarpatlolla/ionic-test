414c065272f27e7fe55d1891701ac6a566028811
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-10-10 22:58:24 +0530

    action sheet removed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController, Platform, ActionSheetController, AlertController, ModalController, ViewController, NavParams } from 'ionic-angular';
+import { NavController, Platform, AlertController, ModalController, ViewController, NavParams } from 'ionic-angular';
 
 
 @Component({
@@ -12,60 +12,11 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     public platform: Platform,
-    public actionsheetCtrl: ActionSheetController,
     public alerCtrl: AlertController,
     public modalCtrl: ModalController
   ) {
     console.log('HomePage constructor');
    }
-
-  openMenu() {
-    let actionSheet = this.actionsheetCtrl.create({
-      title: 'Albums',
-      cssClass: 'action-sheets-basic-page',
-      buttons: [
-        {
-          text: 'Delete',
-          role: 'destructive',
-          icon: !this.platform.is('ios') ? 'trash' : null,
-          handler: () => {
-            console.log('Delete clicked');
-          }
-        },
-        {
-          text: 'Share',
-          icon: !this.platform.is('ios') ? 'share' : null,
-          handler: () => {
-            console.log('Share clicked');
-          }
-        },
-        {
-          text: 'Play',
-          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
-          handler: () => {
-            console.log('Play clicked');
-          }
-        },
-        {
-          text: 'Favorite',
-          icon: !this.platform.is('ios') ? 'heart-outline' : null,
-          handler: () => {
-            console.log('Favorite clicked');
-          }
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel', // will always sort to be on the bottom
-          icon: !this.platform.is('ios') ? 'close' : null,
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        }
-      ]
-    });
-    actionSheet.present()
-
-  }
 
   openAlert() {
     let alert = this.alerCtrl.create({
