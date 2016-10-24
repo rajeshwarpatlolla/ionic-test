6739ff5f43d20dde573dde815da5cb54b7adb7df
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-10-24 16:33:49 +0530

    montha dn year selection dropdowns working

--- src/pages/ionic-datepicker/ionic-datepicker.ts
+++ src/pages/ionic-datepicker/ionic-datepicker.ts
@@ -7,22 +7,45 @@ import { ViewController } from 'ionic-angular';
 export class IonicDatepicker {
   weeks;
   months;
+  years;
   daysList;
   rows;
   cols;
   firstDay;
   selectedDate;
+  selectedMonth;
+  selectedYear;
+  today;
 
   constructor(public viewCtrl: ViewController) {
     console.log('IonicDatepicker Constructor');
     this.selectedDate = new Date();
     this.weeks = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
     this.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    this.years = this.getYearsList();
     this.rows = [0, 7, 14, 21, 28, 35];
     this.cols = [0, 1, 2, 3, 4, 5, 6];
-
+    this.today = this.resetHMSM(new Date());
     let currentDate = new Date();
+    this.selectedMonth = this.months[currentDate.getMonth()];
+    this.selectedYear = currentDate.getFullYear();
     this.loadDaysList(currentDate);
+  }
+
+  resetHMSM(currentDate) {
+    currentDate.setHours(0);
+    currentDate.setMinutes(0);
+    currentDate.setSeconds(0);
+    currentDate.setMilliseconds(0);
+    return currentDate;
+  }
+
+  getYearsList() {
+    let yearsList = [];
+    for (let year = 1900; year <= 2100; year++) {
+      yearsList.push(year);
+    }
+    return yearsList;
   }
 
   loadDaysList(ipDate) {
@@ -44,16 +67,23 @@ export class IonicDatepicker {
 
     firstDay = this.daysList[0].day;
 
-    console.log('firstDay', firstDay);
-    
     for (let j = 0; j < firstDay; j++) {
       this.daysList.unshift({});
     }
-    console.log('this.daysList', this.daysList);
   }
 
-  dismiss() {
-    this.viewCtrl.dismiss();
+  monthSelected(event, selectedMonth) {
+    this.selectedMonth = selectedMonth;
+    this.selectedDate.setMonth(this.months.indexOf(this.selectedMonth));
+    this.selectedDate.setYear(this.selectedYear);
+    this.loadDaysList(new Date(this.selectedDate));
+  }
+
+  yearSelected(event, selectedYear) {
+    this.selectedYear = selectedYear;
+    this.selectedDate.setMonth(this.months.indexOf(this.selectedMonth));
+    this.selectedDate.setYear(this.selectedYear);
+    this.loadDaysList(new Date(this.selectedDate));
   }
 
   dateClicked(dateObj) {
@@ -64,13 +94,32 @@ export class IonicDatepicker {
   prevMonth(dateObj) {
     let date = new Date(dateObj.epoch);
     date.setDate(date.getDate() - dateObj.date);
+    this.selectedMonth = this.months[date.getMonth()];
+    this.selectedYear = date.getFullYear();
     this.loadDaysList(new Date(date));
   }
 
   nextMonth(dateObj) {
-    let date = new Date(dateObj.epoch)
+    let date = new Date(dateObj.epoch);
     date.setDate(date.getDate() + 1);
+    this.selectedMonth = this.months[date.getMonth()];
+    this.selectedYear = date.getFullYear();
     this.loadDaysList(new Date(date));
+  }
+
+  setDate() {
+    console.log('this.selectedDate', this.selectedDate);
+    this.dismiss();
+  }
+
+  setToday() {
+    this.loadDaysList(this.today);
+    this.selectedMonth = this.months[this.today.getMonth()];
+    this.selectedYear = this.today.getFullYear();
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
