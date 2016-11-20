f01c5d48ca00afbbac6b3ecc3ac10e668e5ae0ed
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-11-20 10:39:28 +0530

    html files added for webpack

--- webpack.js
+++ webpack.js
@@ -1,20 +1,25 @@
 module.exports = {
-  entry:  './src/pages/ionic-datepicker',
+  entry: './src/pages/ionic-datepicker',
   output: {
     path: 'build',
-      filename: 'bundle.js',
-    },
+    filename: 'bundle.js',
+  },
   module: {
     loaders: [
       {
         test: /\.js/,
         loader: 'babel',
-        include: __dirname + '/src',
+        include: __dirname + './src/pages/ionic-datepicker',
+      },
+      {
+        test: /\.html?$/,
+        loader: "file?name=[name].[ext]",
+        include: __dirname + './src/pages/ionic-datepicker'
       },
       {
         test: /\.css/,
         loaders: ['style', 'css'],
-        include: __dirname + '/src'
+        include: __dirname + './src/pages/ionic-datepicker'
       }
     ],
   }
